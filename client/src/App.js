e6c72300f1a4d74b24a523b47a85c9c76c563bc0
Miguel <43702390+miguelviera@users.noreply.github.com>
2021-08-07 21:31:20 -0500

    Add files via upload

---
+++ client/src/App.js
@@ -0,0 +1,34 @@
+import React, {useState, useEffect} from 'react'
+import { BrowserRouter } from 'react-router-dom'
+import MainRouter from './MainRouter'
+import {listByUser} from './apis/cart'
+import './App.css';
+
+
+import {AppContext} from './Context'
+import auth from './auth/auth-helper'
+
+function App() {
+  const jwt = auth.isAuthenticated()
+  const [isAuthenticated, userHasAuthenticated] = useState(auth.isAuthenticated());
+  const [itemTotal, setItemTotal] = useState(0)
+
+  useEffect(() => {
+    if(auth.isAuthenticated())
+      listByUser(auth.isAuthenticated().user._id, {t: jwt.token}).then((data) => {
+        setItemTotal(data.length)
+      })
+  }, [])
+  
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <AppContext.Provider value={{ itemTotal, setItemTotal, isAuthenticated, userHasAuthenticated }}>
+          <MainRouter/>
+        </AppContext.Provider>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
