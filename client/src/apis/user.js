7fea73a958e31c8b75b9f1a91f9b14396940d252
Miguel <43702390+miguelviera@users.noreply.github.com>
2021-08-07 21:26:05 -0500

    Add files via upload

---
+++ client/src/apis/user.js
@@ -0,0 +1,89 @@
+import { baseUrl } from './config'
+
+const create = async (user) => {
+  try {
+      let response = await fetch(`${baseUrl}/api/users/`, {
+        method: 'POST',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(user)
+      })
+    return await response.json()
+  } catch(err) {
+    console.log(err)
+  }
+}
+
+const list = async (signal) => {
+  try {
+    let response = await fetch('/api/users/', {
+      method: 'GET',
+      signal: signal,
+    })
+    return await response.json()
+  } catch(err) {
+    console.log(err)
+  }
+}
+
+const read = async (params, credentials, signal) => {
+  try {
+    let response = await fetch('/api/users/' + params.userId, {
+      method: 'GET',
+      signal: signal,
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + credentials.t
+      }
+    })
+    return await response.json()
+  } catch(err) {
+    console.log(err)
+  }
+}
+
+const update = async (userId, credentials, user) => {
+  console.log('userId '  + userId)
+  console.log('user '  + JSON.stringify(user))
+  try {
+    let response = await fetch(`${baseUrl}/api/users/` + userId, {
+      method: 'PUT',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + credentials.t
+      },
+      body: JSON.stringify(user)
+    })
+    return await response.json()
+  } catch(err) {
+    console.log(err)
+  }
+}
+
+const remove = async (params, credentials) => {
+  try {
+    let response = await fetch(`${baseUrl}/api/users/${params.userId}`, {
+      method: 'DELETE',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + credentials.t
+      }
+    })
+    return await response.json()
+  } catch(err) {
+    console.log(err)
+  }
+}
+
+export {
+  create,
+  list,
+  read,
+  update,
+  remove
+}
